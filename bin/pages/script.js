34f0cf4ef99ab978ab7fe0a8c9bafc2b2b8b4ab3
The-pranker <william.y.chen2011@gmail.com>
2026-02-12 10:19:35 -0500

    make pokeball somewhat work

--- bin/pages/script.js
+++ bin/pages/script.js
@@ -1,8 +1,5 @@
-pokeball = 0;
-img_width = 0;
-img_height = 0;
-
-if (pokeball == 0) {
-    img_width = 100;
-    img_height = 100;
+function switchball() {
+  const imageElement = document.getElementById('pokeball');
+  imageElement.src = '/public/pokeballs/greatball.png';
+  imageElement.alt = 'greatball';
 }
